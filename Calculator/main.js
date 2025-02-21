9e4813e441b23a750339e2857a3e7c874b83e7ac
camrinsilva <168480425+camrinsilva@users.noreply.github.com>
2025-02-21 12:45:49 -0600

    Finish calculator project

---
+++ Calculator/main.js
@@ -0,0 +1,47 @@
+let display = document.getElementById('display');
+let isNegative = false;
+
+function appendToDisplay(input) {
+  let lastChar = display.value.slice(-1);
+  if(['+', '-', '*', '/', '%'].includes(lastChar) && ['+', '-', '*', '/', '%'].includes(input)) {
+    return;
+  }
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = '';
+  isNegative = false;
+}
+
+function toggleSign() {
+    if (display.value) {
+      let value = parseFloat(display.value);
+      display.value = value > 0 ? `-${value}` : `${Math.abs(value)}`;
+    }
+}
+
+function calculate() {
+  try {
+    let lastChar = display.value.slice(-1);
+  if(['+', '-', '*', '/', '%'].includes(lastChar)) {
+    throw new Error('Invalid Input');
+  }
+
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+function toggleBrightness() {
+  document.body.classList.toggle('lightmode');
+  let icon = document.querySelector('.brightness-toggle i');
+  icon.classList.add('fa-moon')
+
+  if(document.body.classList.contains('lightmode')) {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
+}
+

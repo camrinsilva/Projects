e498628e06ad07e1ee8a863d5f6395a54f6463ab
camrinsilva <168480425+camrinsilva@users.noreply.github.com>
2025-03-19 14:53:25 -0500

    Finish grid function
    Finish the createGrid function that takes user input and creates a grid with that input

--- EtchASketch/main.js
+++ EtchASketch/main.js
@@ -1,30 +1,63 @@
-const cell = document.querySelectorAll(".cell");
-const input = document.querySelector(".input");
 const blackButton = document.querySelector('.black');
 const randomButton = document.querySelector('.random-color');
 const resetButton = document.querySelector('.reset');
+const enterButton = document.querySelector('.enter');
 
-resetButton.addEventListener('click', function() {
-  cell.forEach(cells => {
-    cells.style.backgroundColor = "white"
-  });
-});
 
-blackButton.addEventListener('click', function() {
-  cell.forEach(cells =>{
-    cells.addEventListener('mouseover', () => {
-    cells.style.backgroundColor = 'black';
-   });
- });
-});
 
-randomButton.addEventListener('click', function() {
-  cell.forEach(cells =>{
-    cells.addEventListener('mouseover', () => {
-    cells.style.backgroundColor = randomColor;
-   });
- });
-});
+
+function createGrid() {
+  const container = document.querySelector('.container');
+  const input = parseInt(document.querySelector("input").value, 10);
+
+
+  container.innerHTML = '';
+  if(!Number.isNaN(input) && input > 0 && input <= 64) {
+  const totalCells = input * input;
+  const cellSize = 500 / input;
+
+    const fragment = document.createDocumentFragment();
+
+    for(let i = 0; i < totalCells; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        fragment.appendChild(cell);
+    } 
+
+    container.appendChild(fragment);
+
+    const cell = document.querySelectorAll(".cell");
+
+    resetButton.addEventListener('click', function() {
+      cell.forEach(cells => {
+        cells.style.backgroundColor = "white";
+        container.innerHTML = '';
+      });
+    });
+    
+    blackButton.addEventListener('click', function() {
+      cell.forEach(cells =>{
+        cells.addEventListener('mouseover', () => {
+        cells.style.backgroundColor = 'black';
+       });
+     });
+    });
+    
+    randomButton.addEventListener('click', function() {
+      cell.forEach(cells =>{
+        cells.addEventListener('mouseover', () => {
+        cells.style.backgroundColor = randomColor;
+       });
+     });
+    });
+  }else {
+    alert("Enter a valid number");
+  }
+}
+
+enterButton.addEventListener('click', createGrid);
 
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
